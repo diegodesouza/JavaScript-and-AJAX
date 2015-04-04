bdbcf446151dead53538e23bb730d145a75916a0
Diego de Souza <diegotrus7@gmail.com>
2015-04-04 13:48:08 -0400

    Reading data with jQuery

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-$('.update:even').load('data.txt');
+$.getJSON('data.json', function(data) {
+  var output = '<ul>';
+  $.each(data, function(key, val) {
+    output += '<li>' + val.name + '</li>';
+  });
+  output += '</ul>';
+  $('#update').append(output);
+});
 
